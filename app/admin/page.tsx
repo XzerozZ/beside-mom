dc2d37aa0bac5941ee43f9a073e29a1fb8a501dc
CosmosDever <aprommhgot@gmail.com>
2025-02-18 18:17:09 +0700

    admin_page_sidebar topbar _allinfomom

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,36 @@
-import Sidebar from '../components/SideBarAdmin'
+"use client";
 
+import { useState } from "react";
+import Sidebar from "../components/SideBarAdmin";
+import AllMomInfoPage from "../components/allmominfopage";
 export default function AdminPage() {
+  const [selectedItem, setSelectedItem] = useState("1"); // Default selected item
+
+  const renderContent = () => {
+    switch (selectedItem) {
+      case "1":
+        return <AllMomInfoPage />;
+      case "2":
+        return <div className="text-black">Content 2</div>;
+      case "3":
+        return <div className="text-black">Content 3</div>;
+      case "4":
+        return <div className="text-black">Content 4</div>;
+      case "5":
+        return <div className="text-black">Content 5</div>;
+      case "6":
+        return <div className="text-black">Content 6</div>;
+      case "7":
+        return <div className="text-black">Content 7</div>;
+      default:
+        return <div>Select an item from sidebar</div>;
+    }
+  };
+
   return (
     <div className="flex bg-gray-100">
-      <Sidebar />
-      <div className="flex-1 p-8">
-        <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
-        {/* Add your main content here */}
-      </div>
+      <Sidebar onItemSelect={setSelectedItem} selectedItem={selectedItem} />
+      <div className="flex-1 p-6">{renderContent()}</div>
     </div>
-  )
+  );
 }
