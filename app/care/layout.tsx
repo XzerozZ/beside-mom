89d45984588d130e102c412f11f8ca5f48eebdcf
Natchapon Ponlaem <natchapon.ponl@kmutt.ac.th>
2025-06-06 17:04:15 +0700

    commit

--- app/care/layout.tsx
+++ app/care/layout.tsx
@@ -4,10 +4,10 @@ import Navbar from '../component/navbar';
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col">
-        <header className="fixed top-0 left-0 w-full">
+        <header className="fixed top-0 left-0 w-full z-10">
           <Navbar />
         </header>
-        <main className="mt-[112px] max-sm:mt-[112px]">
+        <main className="mt-[112px] max-sm:mt-[112px] z-0">
           {children}
         </main>
       </div>
