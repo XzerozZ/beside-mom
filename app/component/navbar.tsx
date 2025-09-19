8355e0d6d03a5fc598c68a6a93c72f2d3d81b4ff
Natchapon Ponlaem <natchapon.ponl@kmutt.ac.th>
2025-09-19 09:52:36 +0700

    add logout in responsive and fix qabox and remove chatmessage

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -13,11 +13,10 @@ const Navbar = () => {
 
   // Helper function to check if nav item is active
   const isActive = (path: string) => {
-    return pathname === path || pathname.startsWith(path + '/');
+    return pathname === path || pathname.startsWith(path + "/");
   };
 
- const fetchMomData = async (id: string, key: string) => {
-   
+  const fetchMomData = async (id: string, key: string) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_url}/user/info/${id}`, {
       headers: {
         Authorization: `Bearer ${key}`,
@@ -51,172 +50,266 @@ const Navbar = () => {
     fetchData();
   }, []);
   return (
-    
     <nav className="bg-[#FFC6CF] flex justify-center fixed top-0 left-0 w-full z-999">
       <div className="flex flex-row justify-between h-[80px] w-[1312px] items-center max-xl:w-[770px] max-sm:w-[324px]">
-      <div className="flex flex-row gap-4 items-center">
-        <Image src="/logo.png" width={48} height={48} alt="Logor"></Image>
-        <a href="/home" className="text-[20px] font-bold text-[#4D4D4D]">Beside Mom</a>
-      </div>
-      <div className="max-xl:hidden">
-        <ul className="flex flex-row gap-[44px]">
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/baby') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/baby">ข้อมูลทารก</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/story') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/story">เรื่องเล่าของคุณแม่</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/care') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/care">การดูแลทารก</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/question') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/question">คำถามที่พบบ่อย</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/calendar') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/calendar">การตรวจตามนัด</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/contact') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/contact">ติดต่อพยาบาล</Link>
-        </li>
-        </ul>
-      </div>
-      <div className="flex flex-row gap-2 max-xl:hidden">
-        <Image
-        src={momData?.image_link || "/profileicon.svg"}
-        width={28}
-        height={28}
-        alt="profile"
-               className="rounded-full w-7 h-7 object-cover"
-
-        ></Image>
-        <div className="relative my-auto">
-        <Image
-          src="/dropdownicon.svg"
-          width={10}
-          height={5}
-          alt="dropdown"
-          className="cursor-pointer"
-          onClick={() => setShowDropdown(!showDropdown)}
-        />
-        {showDropdown && (
-          <div className="absolute right-0 top-5 mt-2 bg-[#FFF4F4] border border-gray-200 rounded-md shadow-lg h-[400px] w-[390px] z-9999">
-          <div className="p-[32px] flex flex-col justify-between h-full">
-            <div className="flex flex-row justify-between">
-            <h1 className="font-bold">โปรไฟล์</h1>
-            <Image
-              src="/x.svg"
-              width={11}
-              height={11}
-              alt="x"
-              onClick={() => setShowDropdown(!showDropdown)}
-            ></Image>
-            </div>
-            <div className="flex flex-col gap-[24px] mx-auto">
-            <Image
-              src={momData?.image_link || "/baby.png"}
-              width={96}
-              height={96}
-              alt="profilepicture"
-                     className="rounded-full w-[96px] h-[96px] object-cover"
-
-            ></Image>
-            <h1 className="font-bold text-[20px]">{momData?.fname} {momData?.lname}</h1>
-            </div>
-            <div className="mx-[70px]">
-          
-            </div>
-            <div>
-              <button
-              className="font-bold text-[20px] text-[#B36868] text-center w-full py-2"
-              onClick={async () => {
-                            const result = await Swal.fire({
-                              title: "คุณแน่ใจหรือไม่?",
-                              text: "คุณจะออกจากระบบ!",
-                              icon: "warning",
-                              showCancelButton: true,
-                              confirmButtonText: "ใช่, ออกจากระบบ!",
-                              cancelButtonText: "ยกเลิก",
-                              confirmButtonColor: "#B36868",
-                            
-                            });
-             
-                           if (result.isConfirmed) {
-                             localStorage.removeItem("token");
-                             localStorage.removeItem("u_id");
-                             localStorage.removeItem("name");
-                             localStorage.removeItem("role");
-                             window.location.href = "/auth/login";
-                           }
-                           }}
-              >
-              ออกจากระบบ
-              </button>
-            </div>
-          </div>
-          </div>
-        )}
+        <div className="flex flex-row gap-4 items-center">
+          <Image src="/logo.png" width={48} height={48} alt="Logor"></Image>
+          <a href="/home" className="text-[20px] font-bold text-[#4D4D4D]">
+            Beside Mom
+          </a>
         </div>
-      </div>
-      <div className="xl:hidden">
-        <Image
-        src="/hamburger.svg"
-        width={27}
-        height={16}
-        alt="hamburger"
-        className="cursor-pointer"
-        onClick={() => setShowDropdown(!showDropdown)}
-        />
-      </div>
+        <div className="max-xl:hidden">
+          <ul className="flex flex-row gap-[44px]">
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/baby") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/baby">ข้อมูลทารก</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/story") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/story">เรื่องเล่าของคุณแม่</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/care") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/care">การดูแลทารก</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/question")
+                  ? "text-[#B36868] font-bold"
+                  : "text-black"
+              }`}
+            >
+              <Link href="/question">คำถามที่พบบ่อย</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/calendar")
+                  ? "text-[#B36868] font-bold"
+                  : "text-black"
+              }`}
+            >
+              <Link href="/calendar">การตรวจตามนัด</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/contact") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/contact">ติดต่อพยาบาล</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-row gap-2 max-xl:hidden">
+          <Image
+            src={momData?.image_link || "/profileicon.svg"}
+            width={28}
+            height={28}
+            alt="profile"
+            className="rounded-full w-7 h-7 object-cover"
+          ></Image>
+          <div className="relative my-auto">
+            <Image
+              src="/dropdownicon.svg"
+              width={10}
+              height={5}
+              alt="dropdown"
+              className="cursor-pointer"
+              onClick={() => setShowDropdown(!showDropdown)}
+            />
+            {showDropdown && (
+              <div className="absolute right-0 top-5 mt-2 bg-[#FFF4F4] border border-gray-200 rounded-md shadow-lg h-[400px] w-[390px] z-9999">
+                <div className="p-[32px] flex flex-col justify-between h-full">
+                  <div className="flex flex-row justify-between">
+                    <h1 className="font-bold">โปรไฟล์</h1>
+                    <Image
+                      src="/x.svg"
+                      width={11}
+                      height={11}
+                      alt="x"
+                      onClick={() => setShowDropdown(!showDropdown)}
+                    ></Image>
+                  </div>
+                  <div className="flex flex-col gap-[24px] mx-auto">
+                    <Image
+                      src={momData?.image_link || "/baby.png"}
+                      width={96}
+                      height={96}
+                      alt="profilepicture"
+                      className="rounded-full w-[96px] h-[96px] object-cover"
+                    ></Image>
+                    <h1 className="font-bold text-[20px]">
+                      {momData?.fname} {momData?.lname}
+                    </h1>
+                  </div>
+                  <div className="mx-[70px]"></div>
+                  <div>
+                    <button
+                      className="font-bold text-[20px] text-[#B36868] text-center w-full py-2"
+                      onClick={async () => {
+                        const result = await Swal.fire({
+                          title: "คุณแน่ใจหรือไม่?",
+                          text: "คุณจะออกจากระบบ!",
+                          icon: "warning",
+                          showCancelButton: true,
+                          confirmButtonText: "ใช่, ออกจากระบบ!",
+                          cancelButtonText: "ยกเลิก",
+                          confirmButtonColor: "#B36868",
+                        });
+
+                        if (result.isConfirmed) {
+                          localStorage.removeItem("token");
+                          localStorage.removeItem("u_id");
+                          localStorage.removeItem("name");
+                          localStorage.removeItem("role");
+                          localStorage.removeItem("chatMessages");
+                          window.location.href = "/auth/login";
+                        }
+                      }}
+                    >
+                      ออกจากระบบ
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="xl:hidden">
+          <Image
+            src="/hamburger.svg"
+            width={27}
+            height={16}
+            alt="hamburger"
+            className="cursor-pointer"
+            onClick={() => setShowDropdown(!showDropdown)}
+          />
+        </div>
       </div>
       {showDropdown && (
-      <div className="xl:hidden bg-[#FFEBEC] w-full h-screen fixed top-0 left-0 z-10">
-        <div className="flex justify-center">
-       <div className="flex flex-row justify-between h-[80px] w-[1312px] items-center max-xl:w-[770px] max-sm:w-[324px]">
-      <div className="flex flex-row gap-4 items-center">
-        <Image src="/logo.png" width={48} height={48} alt="Logo"></Image>
-        <h1 className="text-[20px] font-bold text-[#4D4D4D]">Beside Mom</h1>
-      </div>
-     
-     
-      <div className="xl:hidden">
-        <Image
-        src="/x.svg"
-        width={20}
-        height={16}
-        alt="hamburger"
-        className="cursor-pointer"
-        onClick={() => setShowDropdown(!showDropdown)}
-        />
-      </div>
-      </div>
+        <div className="xl:hidden bg-[#FFEBEC] w-full h-screen fixed top-0 left-0 z-10">
+          <div className="flex justify-center">
+            <div className="flex flex-row justify-between h-[80px] w-[1312px] items-center max-xl:w-[770px] max-sm:w-[324px]">
+              <div className="flex flex-row gap-4 items-center">
+                <Image
+                  src="/logo.png"
+                  width={48}
+                  height={48}
+                  alt="Logo"
+                ></Image>
+                <h1 className="text-[20px] font-bold text-[#4D4D4D]">
+                  Beside Mom
+                </h1>
+              </div>
+
+              <div className="xl:hidden">
+                <Image
+                  src="/x.svg"
+                  width={20}
+                  height={16}
+                  alt="hamburger"
+                  className="cursor-pointer"
+                  onClick={() => setShowDropdown(!showDropdown)}
+                />
+              </div>
+            </div>
+          </div>
+          <ul className="flex flex-col gap-4 p-4 z-10">
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/baby") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/baby">ข้อมูลทารก</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/story") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/story">เรื่องเล่าของคุณแม่</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/care") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/care">การดูแลทารก</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/question")
+                  ? "text-[#B36868] font-bold"
+                  : "text-black"
+              }`}
+            >
+              <Link href="/question">คำถามที่พบบ่อย</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/calendar")
+                  ? "text-[#B36868] font-bold"
+                  : "text-black"
+              }`}
+            >
+              <Link href="/calendar">การตรวจตามนัด</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/contact") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/contact">ติดต่อพยาบาล</Link>
+            </li>
+            <li
+              className={`text-[20px] hover:text-[#B36868] hover:font-bold ${
+                isActive("/profile") ? "text-[#B36868] font-bold" : "text-black"
+              }`}
+            >
+              <Link href="/profile">โปรไฟล์</Link>
+            </li>
+           <li>
+               <button
+                className="font-bold text-[20px] text-[#B36868] text-center w-full py-2"
+                onClick={async () => {
+                  const result = await Swal.fire({
+                    title: "คุณแน่ใจหรือไม่?",
+                    text: "คุณจะออกจากระบบ!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "ใช่, ออกจากระบบ!",
+                    cancelButtonText: "ยกเลิก",
+                    confirmButtonColor: "#B36868",
+                  });
+
+                  if (result.isConfirmed) {
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("u_id");
+                    localStorage.removeItem("name");
+                    localStorage.removeItem("role");
+                    localStorage.removeItem("chatMessages");
+                    window.location.href = "/auth/login";
+                  }
+                }}
+              >
+                ออกจากระบบ
+              </button>
+           
+           </li>
+          </ul>
+          
+           
         </div>
-        <ul className="flex flex-col gap-4 p-4 z-10">
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/baby') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/baby">ข้อมูลทารก</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/story') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/story">เรื่องเล่าของคุณแม่</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/care') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/care">การดูแลทารก</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/question') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/question">คำถามที่พบบ่อย</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/calendar') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/calendar">การตรวจตามนัด</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/contact') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/contact">ติดต่อพยาบาล</Link>
-        </li>
-        <li className={`text-[20px] hover:text-[#B36868] hover:font-bold ${isActive('/profile') ? 'text-[#B36868] font-bold' : 'text-black'}`}>
-          <Link href="/profile">โปรไฟล์</Link>
-        </li>
-        </ul>
-      </div>
       )}
     </nav>
-    
   );
 };
 
--- app/component/qabox.tsx
+++ app/component/qabox.tsx
@@ -3,41 +3,43 @@ import React, { FC, useState } from "react";
 import Image from "next/image";
 import { QuestionAnswer } from "@/app/interface";
 
-const Qabox: FC<QuestionAnswer> = (props:QuestionAnswer) => {
+const Qabox: FC<QuestionAnswer> = (props: QuestionAnswer) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div
-      className={`p-[30px] rounded-xl shadow-md transition-all duration-300 ${
-        isOpen ? "bg-[#FFF4F4]" : "bg-whtie"
-      }`}
-    >
-      <button
-        className="flex w-full justify-between items-center text-left focus:outline-none"
-        onClick={() => setIsOpen(!isOpen)}
-        aria-expanded={isOpen}
-      >
-        <h1 className="text-[16px] font-bold">Q: {props.question}</h1>
-        <Image
-          src={isOpen ? "/up.svg" : "/down.svg"}
-          width={16}
-          height={16}
-          alt="toggle"
-        />
-      </button>
-
+    <button onClick={() => setIsOpen(!isOpen)}>
       <div
-        className={`overflow-hidden transition-all duration-300 ${
-          isOpen ? " mt-4" : "max-h-0"
+        className={`p-[30px] rounded-xl shadow-md transition-all duration-300 ${
+          isOpen ? "bg-[#FFF4F4]" : "bg-whtie"
         }`}
       >
-        <p className="text-gray-700 text-sm">
-          A: <pre className="whitespace-pre-wrap font-noto-sans-thai">
-            {props.answer}
-          </pre>
-        </p>
+        <div
+          className="flex w-full justify-between items-center text-left focus:outline-none"
+          aria-expanded={isOpen}
+        >
+          <h1 className="text-[16px] font-bold">Q: {props.question}</h1>
+          <Image
+            src={isOpen ? "/up.svg" : "/down.svg"}
+            width={16}
+            height={16}
+            alt="toggle"
+          />
+        </div>
+
+        <div
+          className={`overflow-hidden transition-all duration-300 ${
+            isOpen ? " mt-4" : "max-h-0"
+          }`}
+        >
+          <div className="text-gray-700 text-sm text-left">
+            A:{" "}
+            <pre className="whitespace-pre-wrap font-noto-sans-thai">
+              {props.answer}
+            </pre>
+          </div>
+        </div>
       </div>
-    </div>
+    </button>
   );
 };
 
